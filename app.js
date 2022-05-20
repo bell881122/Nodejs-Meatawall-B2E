c85f8ad8809e11239b6ce79afa3ac96a220e455a
emi <bell881122@gmail.com>
2022-05-20 23:23:49 +0800

    init express project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const postRouter = require('./routes/postRouter');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/', postRouter);
+
+app.use(function (req, res, next) {
+  res.status(404).json({
+    status: 'error',
+    message: "無此路由資訊",
+  });
+});
+
+module.exports = app;
