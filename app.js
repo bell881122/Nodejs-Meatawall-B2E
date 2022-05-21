b41dec7e20ff289baca0ec37bd4c2ba3b2a3611c
emi <bell881122@gmail.com>
2022-05-21 19:47:30 +0800

    add 500 Internal Server Errors

--- app.js
+++ app.js
@@ -16,11 +16,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', postRouter);
 
-app.use(function (req, res, next) {
+app.use((req, res) => {
   res.status(404).json({
     status: 'error',
     message: "無此路由資訊",
   });
 });
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send({
+    status: 'error',
+    message: '網站發生錯誤，請稍後再試'
+  })
+})
 
 module.exports = app;
