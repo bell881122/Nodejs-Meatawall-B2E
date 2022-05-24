d8727ab85fcbe9bb798660f81c62ffbb4e6c38c1
emi <bell881122@gmail.com>
2022-05-24 22:36:06 +0800

    調整自訂 error

--- controller/postController.js
+++ controller/postController.js
@@ -13,7 +13,7 @@ async function addPost(req, res) {
   const { content, image } = req.body;
   if (image) {
     if (!image.startsWith('https://')) {
-      return handleError(res, { 'message': '請使用 https 開頭的圖片網址' })
+      return handleError(res, { kind: 'image', message: '請使用 https 開頭的圖片網址' })
     }
   }
   await Post.create({
@@ -28,10 +28,10 @@ async function deletePost(req, res) {
   const { id } = req.params;
   await Post.findByIdAndDelete({ _id: id })
     .then(result => {
-      if (result == null) {
-        return handleError(res, { 'message': '無此 id，請重新確認' })
-      }
-      getPosts(req, res)
+      if (result == null)
+        return handleError(res, { kind: 'id', message: '無此 id，請重新確認' })
+      else
+        getPosts(req, res)
     })
     .catch(err => handleError(res, err));
 };
--- utils/handleError.js
+++ utils/handleError.js
@@ -1,22 +1,33 @@
 const handleError = (res, err) => {
-  console.log(err);
-  let error;
+  let errors = {};
+
+  // 處理 mongoose error
   if (err.errors) {
-    [...Object.keys(err.errors)].forEach(el =>
-      error = err.errors[el].properties?.message ?
-        { ...error, [el]: err.errors[el].properties.message } :
-        { ...error, [el]: err.errors[el].message }
-    );
-  } else if (err.kind === 'ObjectId' && err.name === 'CastError') {
-    error = { message: 'id 有誤，請重新確認' }
-  } else {
-    error = err;
+    [...Object.keys(err.errors)].forEach(el => {
+      errors = err.errors[el].properties?.message ?
+        { ...errors, [el]: err.errors[el].properties.message } :
+        { ...errors, [el]: err.errors[el].message }
+    });
   }
+
+  // 自訂 error
+  if (err.kind) {
+    if (err.kind === 'ObjectId' && err.name === 'CastError') {
+      errors = { ...errors, id: 'id 有誤，請重新確認' }
+    } else {
+      errors = { ...errors, [err.kind]: err.message }
+    }
+  }
+
+  const error = new Error();
+  error.status = 400;
+  error.isOperational = true;
+  error.errors = errors;
+
   res.status(400).json({
     "status": "failed",
-    "error": error
+    error
   })
-  res.end();
 }
 
 module.exports = handleError;
