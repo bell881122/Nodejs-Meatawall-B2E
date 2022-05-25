092ac13e66945b9e7efddd947737a84bcc220f73
emi <bell881122@gmail.com>
2022-05-26 01:06:11 +0800

    取得、新增、刪除貼文，增加身分認證

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const PostController = require('../controller/postController');
 const handleErrorAsync = require('../utils/handleErrorAsync');
+const { isAuth } = require('../utils/auth');
 
-router.get('/posts', handleErrorAsync(PostController.getPosts));
-router.post('/post', handleErrorAsync(PostController.addPost));
-router.delete('/post/:id', handleErrorAsync(PostController.deletePost));
+router.get('/posts', isAuth, handleErrorAsync(PostController.getPosts));
+router.post('/post', isAuth, handleErrorAsync(PostController.addPost));
+router.delete('/post/:id', isAuth, handleErrorAsync(PostController.deletePost));
 
 module.exports = router;
